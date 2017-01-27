938a9a56a5a4ea2b86593b74c3fa6b604839d876
0925227 <stijn_kemp@hotmail.com>
2017-01-27 09:41:21 +0100

    Add files via upload

---
+++ Battleport_main_program.py
@@ -0,0 +1,58 @@
+#running game
+import random
+import math
+import time
+import copy
+import pygame
+
+#PlayerNames = ["empty", "player1", "player2"]
+#BoatKinds = ["size2", "size31", "size32", "size4"]
+#Perks = ["none"]
+
+#import class_Player
+#import class_Boats
+#import class_Positions
+#import class_Game
+import class_Menu
+import class_Visual
+import class_Game
+import class_Player
+import class_Boats
+
+pygame.init()
+
+#start menu  (remove """)
+
+################  BACKGROUND MUSIC  ################
+pygame.mixer.music.load("sound/background_music.wav")
+pygame.mixer.music.play(-1)
+
+
+################  SCREENSIZE  ################
+width = 1280
+height = 720
+size = (width,height)
+
+################  GAME DISPLAY  ################
+gameDisplay = pygame.display.set_mode(size)
+pygame.display.set_caption('Battleport')
+clock = pygame.time.Clock()
+
+################  START MENU  ################
+
+#Menu = class_Menu.Menu(gameDisplay, clock, width, height)
+#Menu.menu_start('main menu')
+
+
+#start game (remove """)
+
+################ START GAME ###################
+
+NewGame = class_Game.Game()
+#CurrentGame = NewGame
+
+
+Visual = class_Visual.Visual(NewGame, gameDisplay, clock, width, height)
+Visual.run('game')
+
+
